2acc27f7aacf484348074697c9a916f1081a0da2
Fantoca.Tako <fantoca@tako.co.id>
2024-10-15 13:46:13 +0700

    Update 0.1.7.6.3

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://aba-nextjs.vercel.app/sitemap.xml",
+    sitemap: "https://armadaberkat.com/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://aba-nextjs.vercel.app",
+      url: "https://armadaberkat.com/",
       //   lastModified: new Date(),
       //   changeFrequency: 'yearly',
       //   priority: 1,
